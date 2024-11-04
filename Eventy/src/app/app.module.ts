d485dc94c6b390b34220597936a736f389ed0960
Matijas Levang <micolevang@gmail.com>
2024-11-04 21:38:05 +0100

    Initial commit

---
+++ Eventy/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LayoutModule } from './layout/layout.module';
+import { WineModule } from './wine/wine.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LayoutModule
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ README.md
@@ -0,0 +1,9 @@
+# Eventy
+This is an event organizer web application made as a project for the Client-side Engineering course in the Faculty of Technical Sciences.
+
+It is currently being developed in **Angular**.
+
+Our team consists of three members:
+- Student 1: Veselin Roganović
+- Student 2: Tamara Cvjetković
+- Student 3: Matijas Levang
