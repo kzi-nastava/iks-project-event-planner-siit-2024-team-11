b2c458c8d7359d15daf763851f4cc462d4e38559
Tamara Cvjetkovic <c.tamara02@gmail.com>
2024-11-18 21:41:23 +0100

    [ADD] '**' routing

--- Eventy/src/app/app-routing.module.ts
+++ Eventy/src/app/app-routing.module.ts
@@ -9,7 +9,8 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'organize-event', component: EventOrganizationComponent}
+  {path: 'organize-event', component: EventOrganizationComponent},
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
