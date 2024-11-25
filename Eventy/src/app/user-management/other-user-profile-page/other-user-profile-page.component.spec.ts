1bd39996155470253e1b9756f238839c4695b831
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2024-11-25 22:44:42 +0100

    [FEAT] + unversioned files

---
+++ Eventy/src/app/user-management/other-user-profile-page/other-user-profile-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OtherUserProfilePageComponent } from './other-user-profile-page.component';
+
+describe('OtherUserProfilePageComponent', () => {
+  let component: OtherUserProfilePageComponent;
+  let fixture: ComponentFixture<OtherUserProfilePageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [OtherUserProfilePageComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(OtherUserProfilePageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Eventy/src/app/user-management/other-user-profile-page/other-user-profile-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-other-user-profile-page',
+  templateUrl: './other-user-profile-page.component.html',
+  styleUrl: './other-user-profile-page.component.css'
+})
+export class OtherUserProfilePageComponent {
+
+}
