c555926e3ff176675ab893d783551393ea6428ee
Matijas Levang <micolevang@gmail.com>
2024-11-27 15:55:50 +0100

    [ADD] Solution category service, with stub values for testing

---
+++ Eventy/src/app/solutions/services/solutions/solution-category.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { environment } from '../../../../env/constants';
+import { CategoryWithId } from '../../model/category-with-id.model';
+import { Category, Status } from '../../model/category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SolutionCategoryService {
+  private controllerURL: string = environment.apiHost + "/api/categories";
+  categories: CategoryWithId[] = [
+    { id: 1, name: 'Category 1', description: 'Description for category 1', status: Status.ACCEPTED },
+    { id: 2, name: 'Category 2', description: 'Description for category 2', status: Status.ACCEPTED },
+    { id: 3, name: 'Category 3', description: 'Description for category 3', status: Status.ACCEPTED },
+    { id: 4, name: 'Category 4', description: 'Description for category 4', status: Status.ACCEPTED },
+    { id: 5, name: 'Category 5', description: 'Description for category 5', status: Status.ACCEPTED },
+    { id: 6, name: 'Category 6', description: 'Description for category 6', status: Status.ACCEPTED },
+    { id: 7, name: 'Category 7', description: 'Description for category 7', status: Status.ACCEPTED },
+    { id: 8, name: 'Category 8', description: 'Description for category 8', status: Status.ACCEPTED },
+    { id: 9, name: 'Category 9', description: 'Description for category 9', status: Status.ACCEPTED },
+    { id: 10, name: 'Category 10', description: 'Description for category 10', status: Status.ACCEPTED }
+ ];
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAll(): Observable<CategoryWithId[]> {
+    //return this.httpClient.get<CategoryWithId[]>(this.controllerURL)
+    return of(this.categories)
+  }
+
+  delete(categoryId: number): void {
+    this.httpClient.delete(this.controllerURL + categoryId);
+  }
+
+  update(category: CategoryWithId): Observable<CategoryWithId> {
+    return this.httpClient.put<CategoryWithId>(this.controllerURL + `/${category.id}`, category);
+  }
+
+  create(category: Category): Observable<CategoryWithId> {
+    return this.httpClient.post<CategoryWithId>(this.controllerURL, category)
+  }
+}
