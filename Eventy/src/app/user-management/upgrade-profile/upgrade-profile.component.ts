630f4eb0322a14ce1c9fdf1ba100dcc368fdfafa
Tamara Cvjetkovic <c.tamara02@gmail.com>
2024-12-14 19:20:48 +0100

    [WIP] upgrade profile

---
+++ Eventy/src/app/user-management/upgrade-profile/upgrade-profile.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+interface ButtonClasses {
+  "role-button" : boolean,
+  "not-selected-button" : boolean
+}
+
+@Component({
+  selector: 'app-upgrade-profile',
+  templateUrl: './upgrade-profile.component.html',
+  styleUrl: './upgrade-profile.component.css'
+})
+export class UpgradeProfileComponent {
+  isOrganizer: boolean = false;
+  registerType: boolean = false;
+
+  setRegisterType(isOrganiser: boolean) : void {
+    this.isOrganizer = isOrganiser;
+  }
+
+  getOrganizerButtonClasses() : ButtonClasses {
+    return {
+      "role-button" : true,
+      "not-selected-button" : !this.isOrganizer
+    }
+  }
+
+  getProviderButtonClasses() : ButtonClasses {
+    return {
+      "role-button" : true,
+      "not-selected-button" : this.isOrganizer
+    }
+  }
+}
