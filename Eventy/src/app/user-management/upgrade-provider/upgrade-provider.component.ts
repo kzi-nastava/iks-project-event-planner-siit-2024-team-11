890b85f5b83fe7c5d61ffb9bb0e133c157e19382
Tamara Cvjetkovic <c.tamara02@gmail.com>
2024-12-15 18:37:08 +0100

    [ADD] upgrade to provider component

---
+++ Eventy/src/app/user-management/upgrade-provider/upgrade-provider.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upgrade-provider',
+  templateUrl: './upgrade-provider.component.html',
+  styleUrl: './upgrade-provider.component.css'
+})
+export class UpgradeProviderComponent {
+
+}
