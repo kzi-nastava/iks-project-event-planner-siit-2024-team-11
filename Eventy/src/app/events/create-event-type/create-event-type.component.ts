27a10aa1c45842c5bceb3a491355b4aa3584eed4
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2024-12-21 19:59:10 +0100

    [FEAT] Connected with create event type

--- Eventy/src/app/events/create-event-type/create-event-type.component.ts
+++ Eventy/src/app/events/create-event-type/create-event-type.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {ICategory} from '../model/events.model';
 import {EventTypeService} from '../event-type.service';
 import {Router} from '@angular/router';
+import {CategoryWithId} from '../../solutions/model/category-with-id.model';
+import {ErrorDialogComponent} from '../../shared/error-dialog/error-dialog.component';
+import {SolutionCategoryService} from '../../solutions/services/solutions/solution-category.service';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-create-event-type',
@@ -13,24 +16,20 @@ export class CreateEventTypeComponent {
   eventTypeFormGroup: FormGroup = new FormGroup({
     name: new FormControl('', Validators.required),
     description: new FormControl('', Validators.required),
-    recommendedCategories: new FormControl([], Validators.required),
+    recommendedSolutionCategoriesIds: new FormControl([], Validators.required),
   });
 
-  categories: ICategory[] = [
-    {
-      name: "Music",
-      description: "Great atmosphere and ambient at your event through MUSIC!"
-    },
-    {
-      name: "Food",
-      description: "Let your guests eat well!"
-    }
-  ];
+  categories: CategoryWithId[] = [];
 
   categoryInputFormControl: FormControl = new FormControl(null, Validators.required);
 
-  constructor(private eventTypeService: EventTypeService, private router: Router) {
-
+  constructor(private eventTypeService: EventTypeService, private router: Router,
+              private categoryService: SolutionCategoryService, private dialog: MatDialog) {
+    this.categoryService.getAll().subscribe({
+      next: (categories: CategoryWithId[]) => {
+        this.categories = categories;
+      }
+    });
   }
 
 
@@ -39,36 +38,62 @@ export class CreateEventTypeComponent {
       this.eventTypeService.add(this.eventTypeFormGroup.value);
       this.router.navigate(['/event-types']);
     } else {
-      // dialog pop up?
+      this.dialog.open(ErrorDialogComponent, {
+        width: '400px',
+        disableClose: true, // prevents closing by clicking outside
+        backdropClass: 'blurred_backdrop_dialog',
+        data: {
+          title: 'Input Error',
+          message: 'Please make sure that all inputs are valid before adding an event type.',
+        },
+      });
     }
   }
 
   addCategory(): void {
-    for(let category of this.eventTypeFormGroup.controls['recommendedCategories'].value) {
-      if(category.name === this.categoryInputFormControl.value.name) {
-        //dialog pop up?
+    for(let categoryId of this.eventTypeFormGroup.controls['recommendedSolutionCategoriesIds'].value) {
+      if(this.findCategoryName(categoryId) === this.categoryInputFormControl.value.name) {
+        this.dialog.open(ErrorDialogComponent, {
+          width: '400px',
+          disableClose: true, // prevents closing by clicking outside
+          backdropClass: 'blurred_backdrop_dialog',
+          data: {
+            title: 'Input Error',
+            message: 'Category already added!',
+          },
+        });
         return;
       }
     }
 
     if(this.categoryInputFormControl.value) {
-      this.eventTypeFormGroup.controls['recommendedCategories'].setValue([
-        ...(this.eventTypeFormGroup.controls['recommendedCategories'].value), this.categoryInputFormControl.value
+      this.eventTypeFormGroup.controls['recommendedSolutionCategoriesIds'].setValue([
+        ...(this.eventTypeFormGroup.controls['recommendedSolutionCategoriesIds'].value), this.categoryInputFormControl.value
       ]);
       this.categoryInputFormControl.reset();
       this.categoryInputFormControl.setErrors(null);
     }
   }
 
-  removeCategory(categoryName: string): void {
-    let newRecommendedCategoryList: ICategory[] = [];
+  removeCategory(id: number): void {
+    let newRecommendedCategoryList: number[] = [];
 
-    for(let category of this.eventTypeFormGroup.controls['recommendedCategories'].value) {
-      if(category.name !== categoryName) {
-        newRecommendedCategoryList.push(category);
+    for(let categoryId of this.eventTypeFormGroup.controls['recommendedSolutionCategoriesIds'].value) {
+      if(categoryId !== id) {
+        newRecommendedCategoryList.push(categoryId);
       }
     }
 
-    this.eventTypeFormGroup.controls['recommendedCategories'].setValue(newRecommendedCategoryList);
+    this.eventTypeFormGroup.controls['recommendedSolutionCategoriesIds'].setValue(newRecommendedCategoryList);
+  }
+
+  findCategoryName(id: number): string {
+    for(let category of this.categories) {
+      if(category.id === id){
+        return category.name;
+      }
+    }
+
+    return null;
   }
 }
