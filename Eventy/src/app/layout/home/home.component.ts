3b7936867d54f56f9e502af4c4751dd6c7acf421
Tamara Cvjetkovic <c.tamara02@gmail.com>
2025-01-08 23:13:43 +0100

    [UPDATE] use data from basebase

--- Eventy/src/app/layout/home/home.component.ts
+++ Eventy/src/app/layout/home/home.component.ts
@@ -10,6 +10,11 @@ export class HomeComponent {
   isEventsSelected : boolean = true;
   currentTab: number = 1;
 
+  filters: any = {}; // To store filters
+  searchQuery: string = ''; // To store search input
+
+  ///////////////////////////////////////////////////////
+
   switchTab(tab: number) : void { 
     if (this.currentTab === 1 && tab === 2) {
       this.currentTab = 2;
@@ -20,6 +25,21 @@ export class HomeComponent {
       this.currentTab = 1;
       this.isEventsSelected = !this.isEventsSelected;
     }
+  }
+
+  // Handle when filters are applied
+  handleFiltersChanged(filters: any): void {
+    this.filters = filters;
+  }
+
+  // Handle when filters are reset
+  handleFiltersReset(): void {
+    this.filters = {}; // Reset filters
+  }
+
+  // Handle search input
+  handleSearch(searchValue: string): void {
+    this.searchQuery = searchValue;
   }
 }
   
