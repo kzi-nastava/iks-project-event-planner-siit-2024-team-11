05e45c5fe18eab868849be0725c93ef0feb1c0a6
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2025-01-26 23:18:27 +0100

    [FEAT] Initial Event Details page look with CSS and loading of the most of the components done

--- Eventy/src/app/events/event-card/event-card.component.ts
+++ Eventy/src/app/events/event-card/event-card.component.ts
@@ -36,7 +36,5 @@ export class EventCardComponent {
       }
     })
   }
-
-  protected readonly event = event;
 }
 
--- Eventy/src/app/events/event-details/event-details.component.ts
+++ Eventy/src/app/events/event-details/event-details.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {EventsService} from '../services/events/events-service.service';
+import {EventDetails} from '../model/events.model';
+import {ActivatedRoute} from '@angular/router';
+import {ErrorDialogComponent} from '../../shared/error-dialog/error-dialog.component';
+import {MatDialog} from '@angular/material/dialog';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-event-details',
@@ -6,5 +12,53 @@ import { Component } from '@angular/core';
   styleUrl: './event-details.component.css'
 })
 export class EventDetailsComponent {
+  private _snackBar = inject(MatSnackBar);
+  event: EventDetails;
 
+  constructor(private eventService: EventsService, private route: ActivatedRoute, private dialog: MatDialog) {
+    let id: number = route.snapshot.params['eventId'];
+    this.eventService.getEvent(id).subscribe({
+      next: (event: EventDetails) => {
+        this.event = event;
+        this.event.agenda = [{
+          name: "Activity",
+          description: "ASMDNJFHBAJDKM asa csd cdsjc jda",
+          location: "Here",
+          startTime: new Date(),
+          endTime: new Date()
+        }];
+      },
+      error: (event: EventDetails) => {
+        this.dialog.open(ErrorDialogComponent, {
+          width: '400px',
+          disableClose: true, // prevents closing by clicking outside
+          backdropClass: 'blurred_backdrop_dialog',
+          data: {
+            title: "Error while loading the event",
+            message: 'Error while loading the event.',
+          },
+        });
+      }
+    });
+  }
+
+  handleFavoriteItem() {
+    this.eventService.toggleFavoriteEvent(this.event.id).subscribe({
+      next: any => {
+        this.event.isFavorite = !this.event.isFavorite;
+        this._snackBar.open("FAVORITE: " + this.event.name, "OK!");
+      },
+      error: any => {
+        this.dialog.open(ErrorDialogComponent, {
+          width: '400px',
+          disableClose: true, // prevents closing by clicking outside
+          backdropClass: 'blurred_backdrop_dialog',
+          data: {
+            title: "Can't like if you aren't logged in",
+            message: 'Please log in to make this your favorite event.',
+          },
+        });
+      }
+    })
+  }
 }
