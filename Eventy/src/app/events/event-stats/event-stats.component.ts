858c417ac18ef6e21f7bc2997bfec53ab4eea566
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2025-01-31 09:36:41 +0100

    [FEAT] Added Event Stats to navigation (Second part of commit)

---
+++ Eventy/src/app/events/event-stats/event-stats.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-stats',
+  templateUrl: './event-stats.component.html',
+  styleUrl: './event-stats.component.css'
+})
+export class EventStatsComponent {
+
+}
