29e479ed9e6db5872cc79f9ca77aadfba452e41f
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2025-02-05 18:47:01 +0100

    [FEAT] Connected everything on add product to work for adding product call to the backend

--- Eventy/src/app/products/add-product/add-product.component.ts
+++ Eventy/src/app/products/add-product/add-product.component.ts
@@ -6,11 +6,11 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {EventTypeService} from '../../events/event-type.service';
 import {SolutionCategoryService} from '../../solutions/services/solutions/solution-category.service';
 import {AuthService} from '../../infrastructure/auth/auth.service';
-import {ServicesService} from '../../services/services/services/services.service';
 import {MatDialog} from '@angular/material/dialog';
 import {Router} from '@angular/router';
-import {CreateService, Service} from '../../services/model/services.model';
 import {ErrorDialogComponent} from '../../shared/error-dialog/error-dialog.component';
+import {ProductService} from '../product.service';
+import {CreateProduct, Product} from '../model/products.model';
 
 @Component({
   selector: 'app-add-product',
@@ -18,23 +18,16 @@ import {ErrorDialogComponent} from '../../shared/error-dialog/error-dialog.compo
   styleUrl: './add-product.component.css'
 })
 export class AddProductComponent {
-  serviceType: string = '';
   allEventTypes: EventTypeCard[] = [];
   allSolutionCategories: CategoryWithId[] = [{id: -1337, name: "New Category", description:"filler text", status: Status.PENDING}];
   selectedFiles: Array<any> = [];
 
-  serviceForm = new FormGroup({
-    serviceCategory: new FormControl(null, [Validators.required]),
+  productFrom: FormGroup = new FormGroup({
+    productCategory: new FormControl(null, [Validators.required]),
     name: new FormControl ('', [Validators.required]),
     description: new FormControl ('', [Validators.required]),
-    specifics: new FormControl ('', [Validators.required]),
-    serviceType: new FormControl ('', [Validators.required]),
-    duration: new FormControl (null, [Validators.required]),
-    minDuration: new FormControl (null, [Validators.required]),
-    maxDuration: new FormControl (null, [Validators.required]),
-    daysNoticeForReservation: new FormControl (null, [Validators.required]),
-    daysNoticeForCancellation: new FormControl (null, [Validators.required]),
-    autoAccept: new FormControl (false),
+    isAvailable: new FormControl (false),
+    isVisible: new FormControl (false),
     relevantEventTypes: new FormControl ([], [Validators.required]),
     price: new FormControl (null, [Validators.required, Validators.min(0)]),
     discount: new FormControl (null, [Validators.required, Validators.min(0), Validators.max(100)]),
@@ -44,48 +37,51 @@ export class AddProductComponent {
   });
 
   constructor(private eventTypeService: EventTypeService, private solutionCategoryService: SolutionCategoryService,
-              private authService: AuthService, private servicesService: ServicesService, private dialog: MatDialog, private router: Router) {
-    this.serviceForm.get('serviceType')?.valueChanges.subscribe(value => {
-      if (value === 'variable') {
-        this.serviceForm.get('duration')?.clearValidators();
-        this.serviceForm.get('minDuration')?.setValidators([Validators.required]);
-        this.serviceForm.get('maxDuration')?.setValidators([Validators.required]);
-      } else {
-        this.serviceForm.get('minDuration')?.clearValidators();
-        this.serviceForm.get('maxDuration')?.clearValidators();
-        this.serviceForm.get('duration')?.setValidators([Validators.required]);
-      }
-
-
-
-      this.serviceForm.get('duration')?.updateValueAndValidity();
-      this.serviceForm.get('minDuration')?.updateValueAndValidity();
-      this.serviceForm.get('maxDuration')?.updateValueAndValidity();
-    });
-
-    this.serviceForm.get('serviceCategory')?.valueChanges.subscribe(value => {
+              private authService: AuthService, private productService: ProductService, private dialog: MatDialog, private router: Router) {
+    this.productFrom.get('productCategory')?.valueChanges.subscribe(value => {
       if (value === -1337) {
-        this.serviceForm.get('newCategoryName')?.setValidators([Validators.required]);
-        this.serviceForm.get('newCategoryDescription')?.setValidators([Validators.required]);
+        this.productFrom.get('newCategoryName')?.setValidators([Validators.required]);
+        this.productFrom.get('newCategoryDescription')?.setValidators([Validators.required]);
       } else {
-        this.serviceForm.get('newCategoryName')?.clearValidators();
-        this.serviceForm.get('newCategoryDescription')?.clearValidators();
+        this.productFrom.get('newCategoryName')?.clearValidators();
+        this.productFrom.get('newCategoryDescription')?.clearValidators();
       }
 
-      this.serviceForm.get('newCategoryName')?.updateValueAndValidity();
-      this.serviceForm.get('newCategoryDescription')?.updateValueAndValidity();
+      this.productFrom.get('newCategoryName')?.updateValueAndValidity();
+      this.productFrom.get('newCategoryDescription')?.updateValueAndValidity();
     });
-  }
 
-  ngOnInit() {
     this.eventTypeService.getActiveEventTypes().subscribe({
       next: (response: EventTypeCard[]) => {
         this.allEventTypes = response;
+      },
+      error: () => {
+        this.dialog.open(ErrorDialogComponent, {
+          width: '400px',
+          disableClose: true, // prevents closing by clicking outside
+          backdropClass: 'blurred_backdrop_dialog',
+          data: {
+            title: "Error while getting event types",
+            message: 'Error while getting event types. Please try again later.',
+          },
+        });
       }
     })
+
     this.solutionCategoryService.getAll().subscribe({
       next: (response: CategoryWithId[]) => {
         this.allSolutionCategories.push(...response);
+      },
+      error: () => {
+        this.dialog.open(ErrorDialogComponent, {
+          width: '400px',
+          disableClose: true, // prevents closing by clicking outside
+          backdropClass: 'blurred_backdrop_dialog',
+          data: {
+            title: "Error while getting product categories",
+            message: 'Error while getting product categories. Please try again later.',
+          },
+        });
       }
     })
   }
@@ -109,56 +105,76 @@ export class AddProductComponent {
     }
   }
 
-  removeFile(index: number): void {
-    this.selectedFiles.splice(index, 1);
-  }
-
-
   submit() {
-    if (this.serviceForm.valid) {
+    if (this.productFrom.valid) {
       let response;
 
-      let newService: CreateService = {
-        name: this.serviceForm.get('name').value,
-        description: this.serviceForm.get('description').value,
-        price: this.serviceForm.get('price').value,
-        discount: this.serviceForm.get('discount').value,
+      let newProduct: CreateProduct = {
+        name: this.productFrom.get('name').value,
+        description: this.productFrom.get('description').value,
+        price: this.productFrom.get('price').value,
+        discount: this.productFrom.get('discount').value,
         imageUrls: this.selectedFiles.map(file => file.preview),
-        providerId: this.authService.getId(),
-        categoryId: this.serviceForm.get('serviceCategory').value === -1337 ? null : this.serviceForm.get('serviceCategory').value,
-        relatedEventTypeIds: this.serviceForm.get('relevantEventTypes').value,
-        specifics: this.serviceForm.get('specifics').value,
-        minReservationTime: this.serviceForm.get('minDuration').value,
-        maxReservationTime: this.serviceForm.get('maxDuration').value,
-        reservationDeadline: this.serviceForm.get('daysNoticeForReservation').value,
-        cancellationDeadline: this.serviceForm.get('daysNoticeForCancellation').value,
-        automaticReservationAcceptance: this.serviceForm.get('autoAccept').value,
+        category: this.productFrom.get('productCategory').value === -1337 ? this.allSolutionCategories[0] : this.allSolutionCategories.find(category => category.id === this.productFrom.get('serviceCategory').value),
+        relatedEventTypes: this.productFrom.get('relevantEventTypes').value,
+        isAvailable: this.productFrom.get('isAvailable').value,
+        isVisible: this.productFrom.get('isVisible').value
       }
 
-      if (this.serviceForm.get('serviceCategory').value === -1337) {
-        let newCategory: Category = {name: this.serviceForm.get('newCategoryName').value, description: this.serviceForm.get('newCategoryDescription').value, status: Status.PENDING};
+      if (this.productFrom.get('productCategory').value === -1337) {
+        let newCategory: Category = {name: this.productFrom.get('newCategoryName').value, description: this.productFrom.get('newCategoryDescription').value, status: Status.PENDING};
         this.solutionCategoryService.create(newCategory).subscribe({
           next: (category: CategoryWithId) => {
-            newService.categoryId = category.id;
-            response = this.servicesService.add(newService);
+            newProduct.category = category;
+            response = this.productService.add(newProduct);
             response.subscribe({
-              next: (createdService: Service) => {
-                this.router.navigate(["my-services"]);
+              next: (product: Product) => {
+                this.router.navigate(["my-profile"]);
+              },
+              error: () => {
+                this.dialog.open(ErrorDialogComponent, {
+                  width: '400px',
+                  disableClose: true,
+                  backdropClass: 'blurred_backdrop_dialog',
+                  data: {
+                    title: 'Input Error',
+                    message: 'Please make sure that all inputs are valid.',
+                  },
+                });
               }
             })
+          },
+          error: () => {
+            this.dialog.open(ErrorDialogComponent, {
+              width: '400px',
+              disableClose: true,
+              backdropClass: 'blurred_backdrop_dialog',
+              data: {
+                title: 'Category Input Error',
+                message: 'Please make sure that all new category inputs are valid.',
+              },
+            });
           }
         })
       } else {
-        response = this.servicesService.add(newService);
+        response = this.productService.add(newProduct);
         response.subscribe({
-          next: (createdService: Service) => {
-            this.router.navigate(["my-services"]);
+          next: (product: Product) => {
+            this.router.navigate(["profile"]);
+          },
+          error: () => {
+            this.dialog.open(ErrorDialogComponent, {
+              width: '400px',
+              disableClose: true,
+              backdropClass: 'blurred_backdrop_dialog',
+              data: {
+                title: 'Input Error',
+                message: 'Please make sure that all inputs are valid.',
+              },
+            });
           }
         })
       }
-      // The reason I have two separate calls towards the add method depends on whether a category must be created first. If yes,
-      // we must ensure that we grab the ID of the new (currently pending) category and use it in our new service
-
     }
     else {
       this.dialog.open(ErrorDialogComponent, {
