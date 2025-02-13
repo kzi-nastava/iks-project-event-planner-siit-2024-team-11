8feacb5068802bde36dfff06ca10c972629da684
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2025-02-13 19:51:50 +0100

    [FEAT] Added initial agenda edit part

--- Eventy/src/app/events/edit-event/edit-event.component.ts
+++ Eventy/src/app/events/edit-event/edit-event.component.ts
@@ -1,14 +1,17 @@
-import {Component} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {EventTypeCard, UpdateEvent} from '../model/events.model';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
+import {Activity, EventTypeCard, UpdateEvent} from '../model/events.model';
 import * as L from 'leaflet';
-import {latLng, LatLng} from 'leaflet';
+import {LatLng} from 'leaflet';
 import {EventTypeService} from '../event-type.service';
 import {provideNativeDateAdapter} from '@angular/material/core';
 import {EventsService} from '../services/events/events-service.service';
 import {ActivatedRoute} from '@angular/router';
 import {MatDialog} from '@angular/material/dialog';
 import {ErrorDialogComponent} from '../../shared/error-dialog/error-dialog.component';
+import {
+  InvalidInputDataDialogComponent
+} from '../../shared/invalid-input-data-dialog/invalid-input-data-dialog.component';
 
 @Component({
   selector: 'app-edit-event',
@@ -53,6 +56,7 @@ export class EditEventComponent {
 
             this.initMap([response.location.latitude, response.location.longitude]);
             this.showNewMapClick(new LatLng(response.location.latitude, response.location.longitude));
+            this.agenda = response.agenda;
           },
           error: () => {
             this.dialog.open(ErrorDialogComponent, {
@@ -116,5 +120,75 @@ export class EditEventComponent {
     this.currentMarker = L.marker(latLng, { icon }).addTo(this.map);
 
     this.mapPinToAddress();
+  }
+
+  activityForm: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    location: new FormControl('', Validators.required),
+    timeRange: new FormControl([], [Validators.required, this.validateTimeRange()])
+  });
+
+  displayedColumns: string[] = ['name', 'description', 'location', 'startTime', 'endTime'];
+  agenda: Activity[] = [];
+
+  minStartTime: Date;
+
+  addActivity() : void {
+    this.activityForm.controls['name'].setValue(this.activityForm.controls['name'].value)
+    this.activityForm.controls['description'].setValue(this.activityForm.controls['description'].value)
+    this.activityForm.controls['location'].setValue(this.activityForm.controls['location'].value)
+    this.activityForm.controls['timeRange'].setValue(this.activityForm.controls['timeRange'].value)
+
+    if(this.activityForm.valid) {
+      let newActivity: Activity = {
+        name: this.activityForm.controls['name'].value,
+        description: this.activityForm.controls['description'].value,
+        location: this.activityForm.controls['location'].value,
+        startTime: this.activityForm.controls['timeRange'].value[0],
+        endTime: this.activityForm.controls['timeRange'].value[1]
+      }
+      this.agenda = [...this.agenda, newActivity];
+      this.minStartTime = this.activityForm.value.timeRange[1];
+      this.activityForm.reset(
+        {
+          "name": "",
+          "description": "",
+          "location": "",
+          "timeRange": [this.minStartTime, this.minStartTime]
+        }
+      );
+      Object.keys(this.activityForm.controls).forEach(key => {
+        this.activityForm.controls[key].setErrors(null) ;
+      });
+    } else {
+      this.dialog.open(InvalidInputDataDialogComponent, {
+        data : {
+          title: "Invalid input",
+          message: "Invalid input data"
+        }
+      });
+    }
+  }
+
+  formatDate(date : Date) : string {
+    return date.toLocaleDateString('en-US', {
+      weekday: 'long',   // full day name (e.g., Monday)
+      year: 'numeric',   // year (e.g., 2024)
+      month: 'long',     // full month name (e.g., October)
+      day: 'numeric',    // day of the month (e.g., 13)
+      hour: '2-digit',   // hour (e.g., 03 PM)
+      minute: '2-digit'  // minute (e.g., 45)
+    });
+  }
+
+  private validateTimeRange(): ValidatorFn {
+    return (): ValidationErrors | null => {
+      if(this.activityForm) {
+        return this.activityForm.controls['timeRange'].value[0] && this.activityForm.controls['timeRange'].value[1] ? null : { bothTimesEntered: true };
+      }
+
+      return null;
+    };
   }
 }
